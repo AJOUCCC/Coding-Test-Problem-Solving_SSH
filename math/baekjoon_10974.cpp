b2b58ce969e3345eba5f74ce9fdd149dfd086435
sosongha3 <sosongha3@ajou.ac.kr>
2025-02-10 11:39:07 +0900

    Feat: permutation question solve

--- math/baekjoon_10974.cpp
+++ math/baekjoon_10974.cpp
@@ -1,16 +1,59 @@
 #include <iostream>
 using namespace std;
 
-int N;
-int nums[9];
-void check(int pos){
+int next_choice(int* nums, int order, int N){
+    //order는 남은 nums에서 order번째로 먼저인 수를 get한다는 뜻
+    int count = 0;
+    for(int i = 0; i < N; i++){
+        if(nums[i] != 0){
+            if(count == order){
+                int result = nums[i];
+                nums[i] = 0;
+                return result;
+            }
+            count++;
+        }
+    }
+    return 0;
+}
+
+void permutation(int* nums, int* perms, int depth, int N){
+
+    //depth == N이라면, 모든 것을 골랐으므로 끝
+    if(depth == N){
+        for(int i = 0; i < N; i++){
+            cout << perms[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
     
-    
+    //하나를 고정하고, 아래 부분들 사전순 조합하기
+    for(int i = 0; i < N-depth; i++){
+
+        //지금 depth에서 선택할 것은 = i번째로 큰 수
+        int next_num = next_choice(nums, i, N);
+        perms[depth] = next_num;
+
+        //하나 선택 후, 다음 depth로 넘어가기
+        permutation(nums, perms, depth+1, N);
+
+        //선택했던 것 원상복구
+        nums[next_num-1] = next_num;
+    }
 }
 
 int main(void){
+    int N;
     cin >> N;
+    int nums[N];
+    int perms[N] = {0};
 
-    //순열 조합하기
+    //N개의 요소 초기화(0~N-1)
+    for(int i = 0; i < N; i++){
+        nums[i] = i+1;
+    }
 
+    permutation(nums, perms, 0, N);
+    
 }
--- math/baekjoon_2407.cpp
+++ math/baekjoon_2407.cpp
@@ -1,19 +1,68 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
+
+string mult(string n1, string n2){
+    //계산을 위한 n2 x n1+n2-1 배열 선언
+    vector<vector<int>> mult_matrix(n2.size(), vector<int>(n1.size() + n2.size() -1, 0));
+    
+    for(int i = n1.size() - 1; i > 0; i--){
+        for(int j = n2.size() - 1; j > 0; j--){
+            int rn1 = n1[i] - '0';
+            int rn2 = n2[j] = '0';
+            mult_matrix[j][i+j] = rn1 * rn2;
+        }
+    }
+
+    //각각의 곱셈된 것을 합치기
+    string new_num;
+    int over_number = 0;
+    for(int i = n1.size() + n2.size() -1; i > 0; i--){
+        int sum = 0;
+        for(int j = n2.size() - 1; j > 0; j--){
+            sum += mult_matrix[j][i];
+        } 
+        sum += over_number;
+
+        if(sum > 10){
+            over_number = sum / 10;
+            sum = sum % 10;
+        } 
+
+        new_num.push_back(sum + '0');
+    }
+
+    return new_num;
+}
+
+char* div(char* n1, char* n2){
+
+}
+
+string fatorial(string n){
+  
+    if(n.size() <= 1 && n[0]-'0' == 0) return n; 
+    else{
+        string lower_str(n);
+        int end = lower_str.size()-1;
+        if(lower_str[end] % 10 != 0){
+            lower_str[end] = lower_str[end] % 10 - 1;
+        }
+        else{
+            lower_str[end-1] = lower_str[end-1] - 1;
+        }
+        return mult(n, factorial(lower_str));
+    }
+}
 
 int main(void){
     int N, M;
-    int smalls[101];
     cin >> N >> M;
 
     //계산 효율을 위한 식 변경
     if(M > 50){
         M = 100 - M;
-    }
-
-    //분자에 올 숫자들 배열에 저장
-    for(int j = 0; j < M; j++){
-        smalls[j+1] = j+1;
     }
 
     //nCm 계산
