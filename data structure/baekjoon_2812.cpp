a93020d6d2a18da5f54e2f3d0eb0f19acc1f6ad4
sosongha3 <sosongha3@ajou.ac.kr>
2025-02-18 11:50:32 +0900

    Feat: making big number with deque solved by solution

--- data structure/baekjoon_2812.cpp
+++ data structure/baekjoon_2812.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <deque>
 using namespace std;
 
 int main(void){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, K;
+    string num;
+    deque<char> dq;
+    cin >> N >> K >> num;
+
+    for(int i = 0; i < N; i++){
+        while(!dq.empty() && dq.back() < num[i] && K > 0){
+            dq.pop_back();
+            K--;
+        }
+        dq.push_back(num[i]);
+    }
     
+    for(int i = 0; i < dq.size() - K; i++){
+        cout << dq[i];
+    }
+    cout << "\n";
 }
