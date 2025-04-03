d40644f661fab23c8a2ead0fb24e4f64da94ecef
sosongha3 <sosongha3@ajou.ac.kr>
2025-04-03 17:35:55 +0900

    Feat: defence game solvation with priority_queue -> success

--- data structure/programmers_디펜스게임.cpp
+++ data structure/programmers_디펜스게임.cpp
@@ -2,43 +2,46 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int solution(int n, int k, vector<int> enemy) {
     int answer = 0;
-    //완전 탐색으로 풀자
+    
     //만약 무적권의 수가 충분하면 모든 라운드 통과
     if(enemy.size() <= k) return enemy.size();
     
-    //아니라면 완전 탐색
-    vector<int> perm = vector<int>(enemy.size(), 1);
+    //우선순위 큐 사용
     int max_round = 0;
-    for(int i = 0; i < k; i++)
-        perm[i] = 0;
-    do{
-        int lives = n;
-        int round = 0;
-        for(int i = 0; i < enemy.size(); i++){
-            if(perm[i] == 0){
-                round++;
-                continue;
-            }
-            
-            if(lives >= enemy[i]){
-                lives -= enemy[i];
-                round++;
-            }
-            else
-                break;
+    priority_queue<int> pq;
+    for(int i = 0; i < enemy.size(); i++){
+        //n으로 처리 가능하면 처리
+        if(n >= enemy[i]){
+            n -= enemy[i];
+            pq.push(enemy[i]);
+            max_round++;
         }
-        // for(int i = 0; i < enemy.size(); i++){
-        //     cout << perm[i] << " ";
-        // }
-        // cout << "\n";
-        // cout << round << "\n";
-        if(round > max_round) max_round = round;
+        //안된다면 k로 무적권 사용
+        else{
+            if(k > 0){
+                if(pq.empty() || pq.top() < enemy[i]){//만약 이전에 지나간 것이 없으면 얘를 무적권 사용
+                    k--;
+                    max_round++;
+                    continue;
+                }
+                else{//이전에 지나간 것이 있다면 그 중 가장 큰 값을 무적권 사용
+                    k--;
+                    n += pq.top();
+                    pq.pop();
+                    i--;
+                    continue;
+                }
+            }
+            else{
+                break;
+            }
+        }
     }
-    while(next_permutation(perm.begin(), perm.end()));
     answer = max_round;
     return answer;
 }
